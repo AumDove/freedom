e1d1c0d5e432154904470824965fe9703cdfde92
aumdove <aumdove13@gmail.com>
2016-08-12 13:39:10 -1000

    created home php for custom blog template page

---
+++ wp-content/themes/pmih/home.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying blog page.
+ *
+ * This is the template that displays blog pages by default.
+ * 
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package PMIH
+ */
+
+get_header(); ?>
+
+<section class="default-page">		
+	<div class="main-content">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<h2 class="entry-title"><?php the_title(); ?></h2>
+			<article class="post-entry">
+				<?php the_content(); ?>
+			</article>
+		<?php endwhile; ?>
+	</div>
+	
+	
+</section>
+
+<?php get_footer(); ?>
